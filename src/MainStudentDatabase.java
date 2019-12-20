6f22afb98800f267bf37430ed7f23936ce64d53d
Akash Ghosh <akashghosh@owners-MacBook-Air.local>
2019-12-20 16:22:28 -0500

    Initial commit

---
+++ src/MainStudentDatabase.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+public class MainStudentDatabase {
+
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+        Student stu1 = new Student();
+        stu1.enroll();
+        stu1.payTuition();
+        System.out.println(stu1.toString());
+
+        //ask how many new students we want to add
+       // System.out.println("Enter number of new students to enroll");
+
+
+     /*   Student [] students = new Student[numOfStudnets];
+        Scanner in = new Scanner(System.in);
+        int numOfStudnets = in.nextInt();
+        for(int n=0; n< numOfStudnets; n++){ */
+
+        }
+    }
